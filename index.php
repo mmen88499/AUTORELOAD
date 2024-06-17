75a8f5ae8b37ddf39584ce757b35960090c79692
mmen88499 <145101430+mmen88499@users.noreply.github.com>
2024-06-17 22:09:03 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Auto-Reloading Page</title>
+  <meta http-equiv="refresh" content="5">
+</head>
+<body>
+
+  <h1>This page will reload automatically every 5 seconds.</h1>
+
+  <iframe src="https://tpr-store.blogspot.com/2024/04/Java-code-to-improve-mobile-phone-performance.html" width="800" height="600"></iframe>
+
+</body>
+</html>
